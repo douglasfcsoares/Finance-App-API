7c148ca1f08c347dbec56281648d8f9e6ddb691b
douglasfcsoares <douglasfcsoares@gmail.com>
2025-12-26 20:42:57 -0300

    feat: add PostresCreateUserRepository

---
+++ src/index.js
@@ -0,0 +1,15 @@
+import 'dotenv/config.js'
+import express from 'express'
+
+import { PostgresHelper } from './db/postgres/helper.js'
+
+const app = express()
+
+app.get('/', async (req, res) => {
+    const results = await PostgresHelper.query('SELECT * FROM users;')
+    res.send(JSON.stringify(results))
+})
+
+app.listen(process.env.PORT, () =>
+    console.log(`Listening on port ${process.env.PORT} ...`),
+)
